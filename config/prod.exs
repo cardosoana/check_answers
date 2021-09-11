71a55047894f86406bb3a4b7df18df0c6d98348b
Ana Cardoso <ana.cardoso@marleyspoon.com>
2021-09-11 13:25:24 -0300

    update config to deploy to gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :check_answers, CheckAnswersWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "polar-everglades-51340.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
